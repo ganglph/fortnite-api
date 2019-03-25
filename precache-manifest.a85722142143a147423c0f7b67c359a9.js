6560bc0dbe5975770027c8d44d73f3458860a141
ganglph <philipp.gangl@hotmail.com>
2019-03-25 13:11:38 +0100

    Updates

---
+++ precache-manifest.a85722142143a147423c0f7b67c359a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7ade41d36201d4172f25b51f37941fc",
+    "url": "/fortnite-api/static/media/icon.d7ade41d.png"
+  },
+  {
+    "revision": "ffee850b59e5f246485f",
+    "url": "/fortnite-api/static/js/runtime~main.426b9e38.js"
+  },
+  {
+    "revision": "ebbe2df8b018c01488cf",
+    "url": "/fortnite-api/static/js/main.6b4005d3.chunk.js"
+  },
+  {
+    "revision": "1d29abcc7cc564987186",
+    "url": "/fortnite-api/static/js/2.19e36c7c.chunk.js"
+  },
+  {
+    "revision": "ebbe2df8b018c01488cf",
+    "url": "/fortnite-api/static/css/main.bbc0eb51.chunk.css"
+  },
+  {
+    "revision": "df402b39884e66316c9139da4ff6389e",
+    "url": "/fortnite-api/index.html"
+  }
+];
